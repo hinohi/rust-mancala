17876a5bf56ca9d8901e70bf87539a02a87722d0
Daiju Nakayama <42.daiju@gmail.com>
2019-02-27 22:00:19 +0900

    総当たりシステム作る

---
+++ src/bin/full_match.rs
@@ -0,0 +1,63 @@
+use mancala_rust::*;
+
+fn ai_factory(s: &str) -> Box<AI> {
+    let s_list = s.split(':').collect::<Vec<_>>();
+    if s_list[0] == "random" {
+        return Box::new(RandomAI::new());
+    } else if s_list[0] == "depth" {
+        let depth = s_list[2].parse().unwrap();
+        if s_list[1] == "diff" {
+            return Box::new(DepthSearchAI::new(ScoreDiffEvaluation::new(), depth));
+        } else {
+            return Box::new(DepthSearchAI::new(PotEvaluation::new(), depth));
+        };
+    } else if s_list[0] == "cut" {
+        let width = s_list[2].parse().unwrap();
+        let depth = s_list[3].parse().unwrap();
+        if s_list[1] == "diff" {
+            return Box::new(CutDepthAI::new(ScoreDiffEvaluation::new(), width, depth));
+        } else {
+            return Box::new(CutDepthAI::new(PotEvaluation::new(), width, depth));
+        };
+    }
+    unreachable!();
+}
+
+fn main() {
+    let list = [
+        "random",
+        "depth:diff:1",
+        "depth:diff:2",
+        "depth:diff:3",
+        "depth:diff:4",
+        "depth:diff:5",
+        "depth:pot:1",
+        "depth:pot:2",
+        "depth:pot:3",
+        "depth:pot:4",
+        "depth:pot:5",
+        "cut:diff:6:1",
+        "cut:diff:6:2",
+        "cut:diff:6:4",
+        "cut:diff:6:6",
+        "cut:diff:12:1",
+        "cut:diff:12:2",
+        "cut:diff:12:4",
+        "cut:pot:6:1",
+        "cut:pot:6:2",
+        "cut:pot:6:4",
+        "cut:pot:6:6",
+        "cut:pot:12:1",
+        "cut:pot:12:2",
+        "cut:pot:12:4",
+    ];
+    loop {
+        for &a in list.iter() {
+            for &b in list.iter() {
+                let mut j = Judge::new(ai_factory(a), ai_factory(b));
+                let (sa, sb) = j.run();
+                println!("{} {} {} {}", a, b, sa, sb);
+            }
+        }
+    }
+}
