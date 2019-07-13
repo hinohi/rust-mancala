8b8069d4922b90949f762b106851c7da7d8761e2
Daiju Nakayama <42.daiju@gmail.com>
2019-07-14 02:01:30 +0900

    full_matchを更新

--- src/bin/full_match.rs
+++ src/bin/full_match.rs
@@ -1,11 +1,30 @@
+use std::env::args;
+
 use mancala_rust::*;
 
 fn main() {
-    let list = ["random", "depth:7", "mctree:10", "mctree:12", "mctree:14"];
+    let list = [
+        "random",
+        "dfs:mc-10:1",
+        "dfs:mc-10:2",
+        "dfs:mc-10:3",
+        "dfs:mc-10:4",
+        "dfs:mc-100:1",
+        "dfs:mc-100:2",
+        "dfs:mc-100:3",
+        "dfs:mc-1000:1",
+        "dfs:mc-1000:2",
+        "dfs:diff:7",
+        "dfs:diff:9",
+        "mctree:10",
+        "mctree:14",
+    ];
+    let args = args().collect::<Vec<_>>();
+    let stealing = args[1].parse().unwrap();
     loop {
         for &a in list.iter() {
             for &b in list.iter() {
-                let mut game = Game::new(false, build_ai(a).unwrap(), build_ai(b).unwrap());
+                let mut game = Game::new(stealing, build_ai(a).unwrap(), build_ai(b).unwrap());
                 let (sa, sb) = game.run();
                 println!("{} {} {} {}", a, b, sa, sb);
             }
