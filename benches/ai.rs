9a26ab90a686732817225864ae8eb6d2a649e8b0
Daiju Nakayama <42.daiju@gmail.com>
2019-07-14 19:15:07 +0900

    [bench] dfsのベンチマークテストを追加
    test dfs5_no_stealing     ... bench:   6,726,174 ns/iter (+/- 1,080,498)
    test dfs5_with_stealing   ... bench:   8,834,435 ns/iter (+/- 177,701)
    test dfs6_no_stealing     ... bench:  26,301,275 ns/iter (+/- 515,373)
    test dfs6_with_stealing   ... bench:  39,221,922 ns/iter (+/- 620,321)
    test mctree_no_stealing   ... bench:     508,161 ns/iter (+/- 22,157)
    test mctree_with_stealing ... bench:     273,855 ns/iter (+/- 14,245)
    test list_next_no_stealing   ... bench:         574 ns/iter (+/- 14)
    test list_next_with_stealing ... bench:         580 ns/iter (+/- 29)

--- benches/ai.rs
+++ benches/ai.rs
@@ -5,7 +5,7 @@ use test::Bencher;
 
 use rand_pcg::Mcg128Xsl64;
 
-use mancala_rust::{Board, MCTree, AI};
+use mancala_rust::{Board, DepthSearchAI, MCTree, ScoreDiffEvaluator, AI};
 
 #[bench]
 fn mctree_with_stealing(b: &mut Bencher) {
@@ -16,5 +16,29 @@ fn mctree_with_stealing(b: &mut Bencher) {
 #[bench]
 fn mctree_no_stealing(b: &mut Bencher) {
     let mut ai = MCTree::new(10, Mcg128Xsl64::new(1));
+    b.iter(|| ai.sow(&Board::new(false)));
+}
+
+#[bench]
+fn dfs5_with_stealing(b: &mut Bencher) {
+    let mut ai = DepthSearchAI::new(ScoreDiffEvaluator::new(), 5);
+    b.iter(|| ai.sow(&Board::new(true)));
+}
+
+#[bench]
+fn dfs5_no_stealing(b: &mut Bencher) {
+    let mut ai = DepthSearchAI::new(ScoreDiffEvaluator::new(), 5);
+    b.iter(|| ai.sow(&Board::new(false)));
+}
+
+#[bench]
+fn dfs6_with_stealing(b: &mut Bencher) {
+    let mut ai = DepthSearchAI::new(ScoreDiffEvaluator::new(), 6);
+    b.iter(|| ai.sow(&Board::new(true)));
+}
+
+#[bench]
+fn dfs6_no_stealing(b: &mut Bencher) {
+    let mut ai = DepthSearchAI::new(ScoreDiffEvaluator::new(), 6);
     b.iter(|| ai.sow(&Board::new(false)));
 }
