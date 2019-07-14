66b974cd9a1b093760dcc4e51d774846f5dff184
Daiju Nakayama <42.daiju@gmail.com>
2019-07-14 11:24:58 +0900

    ベンチマークテストを追加し始める

---
+++ benches/board.rs
@@ -0,0 +1,16 @@
+#![feature(test)]
+
+extern crate test;
+use test::Bencher;
+
+use mancala_rust::Board;
+
+#[bench]
+fn list_next_with_stealing(b: &mut Bencher) {
+    b.iter(|| Board::new(true).list_next());
+}
+
+#[bench]
+fn list_next_no_stealing(b: &mut Bencher) {
+    b.iter(|| Board::new(false).list_next());
+}
