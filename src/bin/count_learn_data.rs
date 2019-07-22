0285101b1f93ac6db40b31ccf3d43bcd69edc1d4
Daiju Nakayama <42.daiju@gmail.com>
2019-07-22 13:14:16 +0900

    評価関数の外部表現方法を変更

--- src/bin/count_learn_data.rs
+++ src/bin/count_learn_data.rs
@@ -1,62 +1,53 @@
 use mancala_rust::learn::*;
 
 fn count_pos1(stealing: bool) {
-    let mut score = [[0; 32]; 12];
-    let mut count = [[0; 32]; 12];
+    let mut score = [[0.0; 32]; 12];
+    let mut count = [[0.0; 32]; 12];
     for (seeds, s, _) in iter_load(stealing).unwrap() {
+        let s = f64::from(s);
         for (pos, seed) in seeds.iter().enumerate() {
-            score[pos][*seed as usize] += s as i64;
-            count[pos][*seed as usize] += 1_i64;
+            let seed = *seed as usize;
+            score[pos][seed] += s;
+            count[pos][seed] += 1.0;
         }
     }
     for pos in 0..12 {
-        print!("[");
         for i in 0..32 {
-            if count[pos][i] < 100 {
-                print!("0.0, ");
+            if count[pos][i] < 3.0 {
+                println!("0");
             } else {
-                print!(
-                    "{:.4}, ",
-                    score[pos][i] as f64 / count[pos][i] as f64 / 12.0
-                );
+                println!("{}", score[pos][i] / count[pos][i] / 12.0);
             }
         }
-        println!("],");
     }
 }
 
 fn count_pos2(stealing: bool) {
-    let mut score = [[[[0; 32]; 12]; 32]; 12];
-    let mut count = [[[[0; 32]; 12]; 32]; 12];
+    let mut score = [[[[0.0; 32]; 12]; 32]; 12];
+    let mut count = [[[[0.0; 32]; 12]; 32]; 12];
     for (seeds, s, _) in iter_load(stealing).unwrap() {
+        let s = f64::from(s);
         for (p1, s1) in seeds.iter().enumerate() {
             let s1 = *s1 as usize;
             for (p2, s2) in seeds.iter().enumerate() {
                 let s2 = *s2 as usize;
-                score[p1][s1][p2][s2] += s as i64;
-                count[p1][s1][p2][s2] += 1_u64;
+                score[p1][s1][p2][s2] += s;
+                count[p1][s1][p2][s2] += 1.0;
             }
         }
     }
     for p1 in 0..12 {
-        println!("[");
         for s1 in 0..32 {
             for p2 in 0..12 {
-                print!("    [");
                 for s2 in 0..32 {
-                    if count[p1][s1][p2][s2] < 10 {
-                        print!("0.0, ");
+                    if count[p1][s1][p2][s2] < 3.0 {
+                        println!("0");
                     } else {
-                        print!(
-                            "{}, ",
-                            score[p1][s1][p2][s2] as f64 / count[p1][s1][p2][s2] as f64 / 144.0
-                        );
+                        println!("{}", score[p1][s1][p2][s2] / count[p1][s1][p2][s2] / 144.0);
                     }
                 }
-                println!("],")
             }
         }
-        println!("],");
     }
 }
 
