0c7667fc0ae2c880170e279091fe0bd0e6c44198
Daiju Nakayama <42.daiju@gmail.com>
2019-07-25 16:11:49 +0900

    random_down_with_weightを実装

--- src/ai/utils.rs
+++ src/ai/utils.rs
@@ -39,6 +39,56 @@ pub fn random_down<R: Rng>(random: &mut R, board: Board) -> Board {
     board
 }
 
+#[inline]
+fn soft_max(x: &mut [f64]) {
+    let max = x.iter().fold(std::f64::NAN, |x, v| x.max(*v));
+    x.iter_mut()
+        .map(|v| {
+            *v = (*v - max).exp();
+        })
+        .last();
+}
+
+#[inline]
+fn choice_with_weight<R: Rng>(random: &mut R, weight: &[f64]) -> usize {
+    let sum = weight.iter().fold(0.0, |x, y| x + *y);
+    let r = random.gen_range(0.0, sum);
+    let mut p = 0.0;
+    for (i, w) in weight.iter().enumerate() {
+        p += *w;
+        if r <= p {
+            return i;
+        }
+    }
+    weight.len() - 1
+}
+
+pub fn random_down_with_weight<R, E>(random: &mut R, eval: &mut E, board: Board) -> Board
+where
+    R: Rng,
+    E: Evaluator,
+    E::Score: Into<f64>,
+{
+    let mut board = board;
+    loop {
+        let mut next_list = board.list_next().drain().collect::<Vec<_>>();
+        if next_list.is_empty() {
+            break;
+        }
+        let prob = {
+            let mut prob = next_list
+                .iter()
+                .map(|board| eval.eval(board).into())
+                .collect::<Vec<_>>();
+            soft_max(&mut prob);
+            prob
+        };
+        let idx = choice_with_weight(random, &prob);
+        board = next_list.swap_remove(idx);
+    }
+    board
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -75,4 +125,41 @@ mod tests {
         let board = random_down(&mut random, Board::new(false));
         assert!(board.is_finished());
     }
+
+    #[test]
+    fn test_soft_max() {
+        let mut x = [-4.0, 10.0, 8.0, 0.0];
+        soft_max(&mut x);
+        for v in x.iter() {
+            assert!(*v >= 0.0);
+        }
+        assert!(x[0] < x[1]);
+        assert!(x[0] < x[2]);
+        assert!(x[0] < x[3]);
+        assert!(x[1] > x[2]);
+        assert!(x[1] > x[3]);
+        assert!(x[2] > x[3]);
+    }
+
+    #[test]
+    fn test_choice_with_weight() {
+        let x = [3.0, 0.0, 7.0];
+        let mut random = Mcg128Xsl64::new(1);
+        let mut count = [0; 3];
+        for _ in 0..1000 {
+            count[choice_with_weight(&mut random, &x)] += 1;
+        }
+        assert!(270 <= count[0] && count[0] <= 330);
+        assert_eq!(count[1], 0);
+        assert!(670 <= count[2] && count[2] <= 730);
+    }
+
+    #[test]
+    fn smoke_random_down_with_weight() {
+        let mut random = Mcg128Xsl64::new(1);
+        let mut eval = ScoreDiffEvaluator::new();
+        let board = random_down_with_weight(&mut random, &mut eval, Board::new(false));
+        assert!(board.is_finished());
+    }
+
 }
