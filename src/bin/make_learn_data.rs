86ecc5533435d5c3db4bb78d9bafb597a2fcacf3
Daiju Nakayama <42.daiju@gmail.com>
2019-08-04 19:49:59 +0900

    学習データ作成スクリプトを約半日で+3億データに収まるようにパラメータ調整

--- src/bin/make_learn_data.rs
+++ src/bin/make_learn_data.rs
@@ -1,13 +1,13 @@
 use rand::SeedableRng;
 use rand_pcg::Mcg128Xsl64;
 
-use mancala_rust::{from_compact_key, learn::*, MCTree};
+use mancala_rust::{learn::*, MCTree};
 
 fn main() {
     let stealing = true;
     let mut ai = MCTree::new(256, Mcg128Xsl64::from_entropy());
     let mut data = load(stealing);
-    for i in 1..=30_000_000 {
+    for i in 1..=300_000 {
         let mut path = to_finish(stealing, &mut ai);
         while let Some(board) = path.pop() {
             if search(&mut data, board, 20).is_none() {
@@ -23,11 +23,8 @@ fn main() {
 
     println!("depth histogram");
     let mut hist = [0; 256];
-    for (key, (score, depth)) in data.iter() {
+    for (_, (_, depth)) in data.iter() {
         hist[*depth as usize] += 1;
-        if *depth > 30 {
-            println!("{:?} depth={} {}", from_compact_key(*key), depth, score);
-        }
     }
     for (depth, count) in hist.iter().enumerate() {
         println!("{} {}", depth, count);
