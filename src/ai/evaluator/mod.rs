7d5f8eb8c5a1ec25f9526f05157110b0ec50695d
Daiju Nakayama <daiju.nakayama@pixiedusttech.com>
2019-11-15 17:10:58 +0900

    evaluatorの構成変更

--- src/ai/evaluator/mod.rs
+++ src/ai/evaluator/mod.rs
@@ -1,46 +1,13 @@
+mod score;
+
 use ndarray::Array1;
 use rand::Rng;
 use rust_nn::predict::{NN4Regression, NN6Regression, Regression};
 
 use super::utils::{random_down, random_down_with_weight};
-use super::{Evaluator, Score};
+use super::Evaluator;
 use crate::board::Board;
-
-impl Score for i32 {
-    const MIN: Self = std::i32::MIN + 1;
-    const MAX: Self = std::i32::MAX - 1;
-    #[inline]
-    fn flip(&self) -> Self {
-        -*self
-    }
-}
-
-impl Score for i8 {
-    const MIN: Self = std::i8::MIN + 1;
-    const MAX: Self = std::i8::MAX - 1;
-    #[inline]
-    fn flip(&self) -> Self {
-        -*self
-    }
-}
-
-impl Score for f64 {
-    const MIN: Self = -1e308;
-    const MAX: Self = 1e308;
-    #[inline]
-    fn flip(&self) -> Self {
-        -*self
-    }
-}
-
-impl Score for f32 {
-    const MIN: Self = -1e38;
-    const MAX: Self = 1e38;
-    #[inline]
-    fn flip(&self) -> Self {
-        -*self
-    }
-}
+pub use score::*;
 
 // -- ScoreDiff
 
@@ -49,7 +16,7 @@ pub struct ScoreDiffEvaluator;
 
 impl ScoreDiffEvaluator {
     pub fn new() -> ScoreDiffEvaluator {
-        ScoreDiffEvaluator {}
+        ScoreDiffEvaluator
     }
 }
 
@@ -99,97 +66,6 @@ impl Evaluator for ScorePosEvaluator {
                 score += POS1_SCORE_MAP[pos + 6][*seed as usize]
             }
             score + f64::from(board.score())
-        }
-    }
-}
-
-// -- WinRate
-
-#[derive(Debug, Copy, Clone, Default)]
-pub struct WinRateScore {
-    win: u32,
-    draw: u32,
-    lose: u32,
-    score: i32,
-}
-
-impl WinRateScore {
-    pub fn new() -> Self {
-        Default::default()
-    }
-
-    pub fn count(&mut self, score: i32) {
-        if score > 0 {
-            self.win += 1;
-        } else if score == 0 {
-            self.draw += 1;
-        } else {
-            self.lose += 1;
-        }
-        self.score += score;
-    }
-
-    pub fn rate(&self) -> (f64, f64, f64) {
-        let n = f64::from(self.win + self.draw + self.lose);
-        (
-            f64::from(self.win) / n,
-            f64::from(self.draw) / n,
-            f64::from(self.score) / n,
-        )
-    }
-}
-
-impl PartialEq for WinRateScore {
-    fn eq(&self, other: &Self) -> bool {
-        self.win == other.win && self.draw == other.draw && self.score == other.score
-    }
-}
-
-impl Eq for WinRateScore {}
-
-impl Ord for WinRateScore {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        (self.win, self.draw, self.score).cmp(&(other.win, other.draw, other.score))
-    }
-}
-
-impl PartialOrd for WinRateScore {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl std::ops::Add for WinRateScore {
-    type Output = WinRateScore;
-    fn add(self, other: Self) -> Self::Output {
-        WinRateScore {
-            win: self.win + other.win,
-            draw: self.draw + other.draw,
-            lose: self.lose + other.lose,
-            score: self.score + other.score,
-        }
-    }
-}
-
-impl Score for WinRateScore {
-    const MIN: Self = WinRateScore {
-        win: 0,
-        draw: 0,
-        lose: 0,
-        score: std::i32::MIN + 1,
-    };
-    const MAX: Self = WinRateScore {
-        win: std::u32::MAX,
-        draw: std::u32::MAX,
-        lose: 0,
-        score: std::i32::MAX - 1,
-    };
-    fn flip(&self) -> Self {
-        WinRateScore {
-            win: self.lose,
-            draw: self.draw,
-            lose: self.win,
-            score: -self.score,
         }
     }
 }
