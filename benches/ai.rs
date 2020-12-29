5a7e8d3caae2a782bee004d2f24e23e212e6646b
Nakayama Daiju <42.daiju@gmail.com>
2020-12-29 21:35:06 +0900

    add NN to bench

--- benches/ai.rs
+++ benches/ai.rs
@@ -2,7 +2,9 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 use rand_pcg::Mcg128Xsl64;
 
-use mancala_rust::{Board, DepthSearchAI, MCTree, ScoreDiffEvaluator, AI};
+use mancala_rust::{
+    Board, DepthSearchAI, MCTree, NN4Evaluator, NN6Evaluator, ScoreDiffEvaluator, AI,
+};
 
 fn mctree_with_stealing(c: &mut Criterion) {
     let mut ai = MCTree::new(10, Mcg128Xsl64::new(1));
@@ -46,6 +48,16 @@ fn dfs6_no_stealing(c: &mut Criterion) {
     });
 }
 
+fn nn4_dfs2(c: &mut Criterion) {
+    let mut ai = DepthSearchAI::new(NN4Evaluator::new(true), 2);
+    c.bench_function("nn4_dfs2", |b| b.iter(|| ai.sow(&Board::new(true))));
+}
+
+fn nn6_dfs2(c: &mut Criterion) {
+    let mut ai = DepthSearchAI::new(NN6Evaluator::new(true), 2);
+    c.bench_function("nn6_dfs2", |b| b.iter(|| ai.sow(&Board::new(true))));
+}
+
 criterion_group!(
     benches,
     mctree_with_stealing,
@@ -54,5 +66,7 @@ criterion_group!(
     dfs5_no_stealing,
     dfs6_with_stealing,
     dfs6_no_stealing,
+    nn4_dfs2,
+    nn6_dfs2,
 );
 criterion_main!(benches);
